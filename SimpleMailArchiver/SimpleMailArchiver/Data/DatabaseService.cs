0645b26f1f7255b4de66c3531f0d5b94152e559d
ax-meyer <32029723+ax-meyer@users.noreply.github.com>
2022-06-18 14:28:15 +0200

    Cleaned up project file.

--- SimpleMailArchiver/SimpleMailArchiver/Data/DatabaseService.cs
+++ SimpleMailArchiver/SimpleMailArchiver/Data/DatabaseService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 namespace SimpleMailArchiver.Data
 {
     public class DatabaseService
